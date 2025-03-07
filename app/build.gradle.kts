bedc0a2e20d826363d3dd8dd18b805c5c8028af7
YasserCBF <1520478@senati.pe>
2025-03-07 11:07:02 -0500

    casi finalizacion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.invetario_gue_lp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.invetario_gue_lp"
